a41c70bdc5f3ac8fcdf8494c070360b518768093
Jo-Blade <bladeur13@free.fr>
2025-12-02 17:03:20 +0100

    Day 2 part 2 2025

--- 2025/bin/day2.ml
+++ 2025/bin/day2.ml
@@ -20,6 +20,9 @@ let rec sized_int_seq_builder n : sized_int Seq.t =
 let invalid_ids_seq : int Seq.t =
   Seq.map (fun (n, p10) -> (n * p10) + n) @@ sized_int_seq_builder (1, 10)
 
+let invalid_ids_part1 max_inter_val =
+  Seq.take_while (fun x -> x <= max_inter_val) invalid_ids_seq
+
 (** A type to represent the intervals *)
 type interval = Interval of int * int
 
@@ -28,7 +31,8 @@ let in_interval (x : int) (Interval (n1, n2)) = x >= n1 && x <= n2
 
 (** Keep only the invalid ids that belong to at least one interval and sum them
 *)
-let day2_part1 (inter_seq : interval Seq.t) =
+let sum_invalid_ids (invalid_ids_seq : int -> int Seq.t)
+    (inter_seq : interval Seq.t) =
   let inter_l = List.of_seq inter_seq in
   let
       (* the max possible value of all intervals *)
@@ -38,8 +42,34 @@ let day2_part1 (inter_seq : interval Seq.t) =
   in
   Seq.filter_map (fun x ->
       List.find_map (fun i -> if in_interval x i then Some x else None) inter_l)
-  @@ Seq.take_while (fun x -> x < max_inter_val) invalid_ids_seq
+  @@ invalid_ids_seq max_inter_val
   |> Seq.fold_left ( + ) 0
+
+(** Compute the finite sequence of all concatenations of n < p10 that are
+    smaller than max
+    @param max the maximum value to stop the sequence
+    @param n the first part of the sized_int (n, p10)
+    @param n the second part of the sized_int (n, p10)
+    @param acc the accumulator, MUST BE INITIALIZED TO N *)
+let rec all_concat_n_seq max n p10 acc =
+ fun () ->
+  if acc <= max / p10 then
+    Seq.Cons ((acc * p10) + n, all_concat_n_seq max n p10 ((acc * p10) + n))
+  else Seq.Nil
+
+(** For each sized_int compute all its possible concatenations and then remove
+    doubles *)
+let invalid_ids_part2 max_inter_val : int Seq.t =
+  Seq.flat_map (fun (n, p10) -> all_concat_n_seq max_inter_val n p10 n)
+  @@ Seq.take_while (fun (n, p10) -> n <= max_inter_val / p10)
+  @@ sized_int_seq_builder (1, 10)
+  |>
+  (* Remove doubles in the result *)
+  fun xs ->
+  List.sort_uniq Int.compare @@ List.of_seq xs
+  |>
+  (* Match function signature *)
+  List.to_seq
 
 (** The parser for today's input *)
 let p =
@@ -47,4 +77,10 @@ let p =
   sep_by (char ',')
   @@ combine2 (fun n1 n2 -> Interval (n1, n2)) (uint +<- char '-') uint
 
-let () = day2_part1 @@ P.run_on_argv1 p |> Printf.printf "solution part1 = %d\n"
+let () =
+  sum_invalid_ids invalid_ids_part1 @@ P.run_on_argv1 p
+  |> Printf.printf "solution part1 = %d\n"
+
+let () =
+  sum_invalid_ids invalid_ids_part2 @@ P.run_on_argv1 p
+  |> Printf.printf "solution part1 = %d\n"
