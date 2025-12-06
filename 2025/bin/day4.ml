33ca33e9466cddb3c75bbe4231bdd43124675e64
Jo-Blade <bladeur13@free.fr>
2025-12-06 11:49:39 +0100

    Day 4 part 2 2025

--- 2025/bin/day4.ml
+++ 2025/bin/day4.ml
@@ -5,20 +5,29 @@ type grid_content = Roll | Empty
 
 (** Test if the position pos in the grid contains a roll, which is accessible by
     the forklift *)
-let is_accessible_roll grid_content (pos : Vec.vec2) =
+let is_accessible_roll grid_content (pos : Vec2.t) =
   if grid_content pos = Roll then
-    List.map grid_content @@ Vec.cross pos @ Vec.diag pos
+    List.map grid_content @@ Vec2.cross pos @ Vec2.diag pos
     |> List.filter (function Roll -> true | Empty -> false)
     |> List.length
     |> fun x -> x < 4
   else false
 
-let day4_part1 ((lines, columns), grid_content) =
-  List.init lines (fun i ->
-      List.init columns (fun j ->
-          Bool.to_int @@ is_accessible_roll grid_content (i, j)))
-  |> List.map @@ List.fold_left ( + ) 0
-  |> List.fold_left ( + ) 0
+(** Compute the list of the positions of all accessible rolls in the given grid
+    of dimensions grid_dim *)
+let all_accessible_rolls (grid_dim, grid_content) =
+  List.filter (is_accessible_roll grid_content) @@ Vec2.square (0, 0) grid_dim
+
+(** Recursively remove all accessible rolls and count the number of removals *)
+let day4_part2 (grid_dim, grid_content) =
+  let rec aux n grid =
+    match all_accessible_rolls (grid_dim, Grids.naive_of_mapgrid grid) with
+    | [] -> n
+    | l ->
+        aux (n + List.length l)
+        @@ List.fold_left (fun acc a -> Grids.set a Empty acc) grid l
+  in
+  aux 0 (Grids.init_mapgrid grid_content)
 
 (** The parser for today's input *)
 let p =
@@ -29,4 +38,9 @@ let p =
   | _ -> None
 
 let () =
-  day4_part1 @@ Parsers.run_on_argv1 p |> Printf.printf "solution part1 = %d\n"
+  all_accessible_rolls @@ Parsers.run_on_argv1 p
+  |> List.length
+  |> Printf.printf "solution part1 = %d\n"
+
+let () =
+  day4_part2 @@ Parsers.run_on_argv1 p |> Printf.printf "solution part2 = %d\n"
